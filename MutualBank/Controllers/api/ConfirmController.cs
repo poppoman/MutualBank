8ececae18f07d951e94c95d2c919623de56ab271
DESKTOP-KT97FAF\lolerve <lolerve860217@gmail.com>
2022-07-11 16:11:50 +0800

    Login

---
+++ MutualBank/Controllers/api/ConfirmController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MutualBank.Models;
+
+namespace MutualBank.Controllers.api
+{
+    [Route("api/Confirm/{action}")]
+    [ApiController]
+    public class ConfirmController : ControllerBase
+    {
+        private readonly MutualBankContext _mutualBankContext;
+
+        public ConfirmController(MutualBankContext mutualBankContext)
+        {
+            _mutualBankContext = mutualBankContext;
+        }
+
+        [HttpGet]
+        public async Task<string> ConfirmAccount(string id)
+        {
+            var user = _mutualBankContext.Logins.FirstOrDefault(u => u.LoginName == id);
+            if (user == null) return "OK";
+            else return "有人使用";
+        }
+    }
+}
