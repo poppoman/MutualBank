92d42e48224764dc19649d9884b3664f4e3c239f
阿暉伯 <poppoman@gmail.com>
2022-07-12 18:04:30 +0800

    測試

--- MutualBank/Models/MutualBankContext.cs
+++ MutualBank/Models/MutualBankContext.cs
@@ -22,7 +22,7 @@ namespace MutualBank.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+
                 optionsBuilder.UseSqlServer("Data Source=61.216.13.147,18349;Initial Catalog=MutualBank;Persist Security Info=True;User ID=TGM101;Password=TGM@5832");
             }
         }
