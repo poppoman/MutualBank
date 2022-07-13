5ea3c7f75f9b6f15693512435a036968fc41f8e7
Chao <azh1230@gmail.com>
2022-07-13 15:57:33 +0800

    新增卡片清單(個人頁)

---
+++ MutualBank/Controllers/CaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MutualBank.Models;
+
+namespace MutualBank.Controllers
+{
+    public class CaseController : Controller
+    {
+        private readonly MutualBankContext _mutualBankContext;
+        public CaseController(MutualBankContext MutualBankContext)
+        {
+            _mutualBankContext = MutualBankContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetPartialPostCase() 
+        {
+            return PartialView("PostCase");
+        }
+        public IActionResult GetPartialCaseList()
+        {
+            return PartialView("CaseList");
+        }
+    }
+}
---
+++ MutualBank/wwwroot/js/PostCaseReady.js
@@ -0,0 +1,31 @@
+﻿
+//-設定預約貼文可選取的時間(暫定14天內)
+    let currentDate = new Date();
+    function getDateString(date) {
+        let monthJs = date.getMonth();
+    let monthMd = (monthJs + 1) < 10 ? `0${monthJs + 1}` : monthJs + 1 > 12 ? monthJs : monthJs + 1;
+    let dateJs = date.getDate();
+    let dateMd = dateJs < 10 ? `0${dateJs}` : dateJs;
+    return `${date.getFullYear()}-${monthMd}-${dateMd}`;
+        }
+
+    //計算日曆可選取的最大時間
+    function FinalDate(dateBegin, intCounts) {
+        let selectedTimeStamp = new Date(dateBegin).getTime();
+    let resultTimeStamp = selectedTimeStamp + intCounts * 1000 * 60 * 60 * 24;
+    let finalDay = new Date(resultTimeStamp);
+    return getDateString(finalDay);
+        }
+
+    //初始化日曆
+    $(document).ready(function () {
+        //設定日期預設顯示與最大、小值
+        dateApp.min = dateApp.value = getDateString(currentDate);
+    dateApp.max = FinalDate(currentDate, 14);
+    //下架時間提醒
+    dateRemind.innerText = dateApp.max;
+
+    //預設關閉預約發佈時間
+    dateApp.setAttribute('disabled', 'disabled');
+    timeApp.setAttribute('disabled', 'disabled');
+        });
