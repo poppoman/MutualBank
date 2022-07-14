9cc9d967e12d302819bc7a8d88ecd478faabd6e9
Fay <phlpoi710482@gmail.com>
2022-07-14 14:11:54 +0800

    新增postpage

---
+++ MutualBank/Controllers/PostPageController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MutualBank.Models;
+
+namespace MutualBank.Controllers
+{
+    public class PostPageController : Controller
+    {
+        private readonly MutualBankContext _context;
+
+        public PostPageController(MutualBankContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PostPage
+        public async Task<IActionResult> Index()
+        {
+              return _context.Messages != null ? 
+                          View(await _context.Messages.ToListAsync()) :
+                          Problem("Entity set 'MutualBankContext.Messages'  is null.");
+        }
+
+        // GET: PostPage/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Messages == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages
+                .FirstOrDefaultAsync(m => m.MsgId == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // GET: PostPage/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+  
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MsgId,MsgAddDate,MsgCaseId,MsgUserId,MsgContent")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(message);
+        }
+
+        // GET: PostPage/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Messages == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            return View(message);
+        }
+
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MsgId,MsgAddDate,MsgCaseId,MsgUserId,MsgContent")] Message message)
+        {
+            if (id != message.MsgId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(message);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MessageExists(message.MsgId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(message);
+        }
+
+        // GET: PostPage/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Messages == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages
+                .FirstOrDefaultAsync(m => m.MsgId == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // POST: PostPage/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Messages == null)
+            {
+                return Problem("Entity set 'MutualBankContext.Messages'  is null.");
+            }
+            var message = await _context.Messages.FindAsync(id);
+            if (message != null)
+            {
+                _context.Messages.Remove(message);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MessageExists(int id)
+        {
+          return (_context.Messages?.Any(e => e.MsgId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ MutualBank/wwwroot/js/PostPage.js
@@ -0,0 +1,87 @@
+﻿let vm = new Vue({
+
+    el:"#postpagebody"
+
+
+})
+
+//新增留言
+$(".messageboard-frame").on("click", "button:eq(0)", function () {
+    var textareaVal = $("textarea:eq(0)").val()
+    $(".messageboard-frame").append("<div class='respone-mes-frame'></div>")
+    $(".respone-mes-frame").last().append("<div class='responer-pic'></div> <div class='pad-content'></div>")
+    $(".responer-pic").last().append("<a href=''><div class='pad-avatar'><img src='background.jpg' alt='無此圖片' class='respons-picture'></div></a>")
+    $(".pad-content").last().append("<div><div class='responser-name'>留言者1</div></div><div class='responser-context'><div class='text'></div></div><div class='response-day-button'><i class='fa-regular fa-clock'></i><span class='post-time'>XX 天前</span><span class='replay-btn'>回覆</span>")
+    $(".text").last().append(textareaVal)
+
+    $("textarea:eq(0)").val("");
+    $("button:eq(0)").attr("disabled", "true");
+
+
+})
+
+//新增回覆留言
+$(".messageboard-frame").on("click", "button:eq(2)", function () {
+
+    var reextareaVal = $("textarea:eq(1)").val()
+    var fapadcontent = $("button:eq(2)").parents(".pad-content")
+
+    fapadcontent.append("<div class='comment-bother-frame'><div class='comment-bother'><a href='' class='comment-pic-frame'><div style='width: 24px; height: 24px;'' class='avatar-frame'><img src='background.jpg' alt='無此圖片' class='comment-bother-pic'></div></a><div class='comment-brother-re'><div class='responser-name-frame'><a class='responser-name' href='''>回覆者的名字</a><div class='reply-user'><span>回覆給：</span><div class='comment-bother'><a href=''><img src='background.jpg' alt='無此圖片'class='comment-bother-pic'></a></div><a>名稱</a></div></div> <div class='text'></div><div class='response-day-button'><i class='fa-regular fa-clock'></i><span class='post-time'>XX 天前</span><span class='replay-btn'>回覆</span></div></div></div></div>")
+
+    fapadcontent.find(".text").last().append(reextareaVal)
+    $("textarea:eq(1)").val("");
+    $("button:eq(2)").attr("disabled", "true");
+    $(".comment-bother-frame-input").remove()
+
+})
+
+//按回覆後新增留言區 
+$(".messageboard-frame").on("click", "[class=replay-btn]", function (e) {
+    var a = $(e.target).parents(".comment-bother-frame")
+    var b = $(e.target).parents(".pad-content")
+
+    if ($(".textarea-box").length == 1 & a.length >= 1) {
+        a.after("<div class='comment-bother-frame-input'></div>")
+        $(".comment-bother-frame-input").append("<div class='comment-bother-input'></div)")
+        $(".comment-bother-input").last().append("<a href='' class='comment-pic-frame'></a><div class='textarea-box'></div><div class='col-auto-input'></div>")
+        $(".comment-pic-frame").last().append("<div style='width: 24px; height: 24px;' class='avatar-frame'></div>")
+        $(".avatar-frame").last().append(" <img src='background.jpg' alt='無此圖片' class='comment-bother-pic'>")
+        $(".textarea-box:eq(1)").append("<textarea type='text' class='form-control' id='exampleFormControlTextarea1' placeholder='請輸入你的訊息' maxlength='200' rows='3'></textarea><div class='statics-text'>0/200字</div>")
+        $(".col-auto-input").append(" <button type='button' class='btn btn-primary' disabled data-bs-toggle='button' id='1'>留言</button>")
+    }
+
+    if ($(".textarea-box").length == 1 & b.length >= 1) {
+        b.append("<div class='comment-bother-frame-input'></div>")
+        $(".comment-bother-frame-input").append("<div class='comment-bother-input'></div)")
+        $(".comment-bother-input").last().append("<a href='' class='comment-pic-frame'></a><div class='textarea-box'></div><div class='col-auto-input'></div>")
+        $(".comment-pic-frame").last().append("<div style='width: 24px; height: 24px;' class='avatar-frame'></div>")
+        $(".avatar-frame").last().append(" <img src='background.jpg' alt='無此圖片' class='comment-bother-pic'>")
+        $(".textarea-box:eq(1)").append("<textarea type='text' class='form-control' id='exampleFormControlTextarea1' placeholder='請輸入你的訊息' maxlength='200' rows='3'></textarea><div class='statics-text'>0/200字</div>")
+        $(".col-auto-input").append(" <button type='button' class='btn btn-primary' disabled data-bs-toggle='button' id='1'>留言</button>")
+    }
+
+    $(".pad-content").on("input propertychange", "[class=form-control]", function () {
+        $("button:eq(2)").removeAttr("disabled");
+    })
+    $(".pad-content").on("keyup", "[class=form-control]", function () {
+        if ($("textarea:eq(1)").val() == "") {
+            $("button:eq(2)").attr("disabled", "true");
+        }
+    })
+
+});
+
+
+//偵測輸入=>按鈕啟動
+$(".messageboard-frame").on("input propertychange", "[class=form-control]:eq(0)", function () {
+    $("button:eq(0)").removeAttr("disabled");
+})
+
+//偵測鍵盤UP沒有值=>按鈕關閉    
+$(".messageboard-frame").on("keyup", "[class=form-control]:eq(0)", function () {
+    if ($("textarea:eq(0)").val() == "") {
+        $("button:eq(0)").attr("disabled", "true");
+    }
+})
+
+
