f72b0578e2a98dbe27fb5f14a077118f3daa9c5f
GailChang <107825061+GailChang@users.noreply.github.com>
2022-07-20 15:58:40 +0800

    增加後臺管理

---
+++ MutualBank/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MutualBank.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MutualBank/Areas/Admin/Controllers/SkillsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MutualBank.Models;
+
+namespace MutualBank.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SkillsController : Controller
+    {
+        private readonly MutualBankContext _context;
+
+        public SkillsController(MutualBankContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Skills
+        public async Task<IActionResult> Index()
+        {
+              return _context.Skills != null ? 
+                          View(await _context.Skills.ToListAsync()) :
+                          Problem("Entity set 'MutualBankContext.Skills'  is null.");
+        }
+
+        // GET: Admin/Skills/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Skills == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.SkillId == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
+        }
+
+        // GET: Admin/Skills/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Skills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SkillId,SkillName")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(skill);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skill);
+        }
+
+        // GET: Admin/Skills/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Skills == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Admin/Skills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SkillId,SkillName")] Skill skill)
+        {
+            if (id != skill.SkillId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(skill);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SkillExists(skill.SkillId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skill);
+        }
+
+        // GET: Admin/Skills/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Skills == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.SkillId == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
+        }
+
+        // POST: Admin/Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Skills == null)
+            {
+                return Problem("Entity set 'MutualBankContext.Skills'  is null.");
+            }
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill != null)
+            {
+                _context.Skills.Remove(skill);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SkillExists(int id)
+        {
+          return (_context.Skills?.Any(e => e.SkillId == id)).GetValueOrDefault();
+        }
+    }
+}
