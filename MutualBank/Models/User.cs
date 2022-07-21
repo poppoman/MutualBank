14a14279ffcaf27daa4e12b4ff15781c90138e3a
OldYang <homkins8@gmail.com>
2022-07-21 11:53:18 +0800

    個人頁面資料修改，新增讀取資料庫功能 Vue

--- MutualBank/Models/User.cs
+++ MutualBank/Models/User.cs
@@ -5,10 +5,6 @@ namespace MutualBank.Models
 {
     public partial class User
     {
-        public User()
-        {
-            Cases = new HashSet<Case>();
-        }
         public int UserId { get; set; }
         public string UserLname { get; set; } = null!;
         public string UserFname { get; set; } = null!;
@@ -24,7 +20,5 @@ namespace MutualBank.Models
         public string? UserSchool { get; set; }
         public string? UserFaculty { get; set; }
         public int UserPoint { get; set; }
-
-        public virtual ICollection<Case> Cases { get; set; }
     }
 }
