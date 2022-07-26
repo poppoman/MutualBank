ff5acb6fb6b0c55c0a1c996203f0eb30a0e21851
Fay <phlpoi710482@gmail.com>
2022-07-26 19:04:52 +0800

    更新 postpage 留言板

--- MutualBank/wwwroot/js/PostPage.js
+++ MutualBank/wwwroot/js/PostPage.js
@@ -1,32 +1,36 @@
 ﻿
 //新增留言
 $(".messageboard-frame").on("click", "#commentbtn", function () {
+    /*var id = location.pathname.replace("/postpage/index/", "")*/
+    var id = document.getElementById("commentbtn").dataset.caseid;
+    var formData = new FormData();
+    formData.append("id", id);
+    formData.append("content", $("#exampleFormControlTextarea1").val());
+    $.ajax({
+        url: "/PostPage/AddComment",
+        type: "POST",
+        data: formData,
+        async: false,
+        cache: false,
+        contentType: false,
+        enctype: 'multipart/form-data',
+        processData: false,
+        }).done(function (res) {
+           console.log(res)
+        })
+         .fail(function (res) {
+            console.log(res);
+         });
     var textareaVal = $("textarea:eq(0)").val()
     $(".messageboard-frame").append("<div class='respone-mes-frame'></div>")
     $(".respone-mes-frame").last().append("<div class='responer-pic'></div> <div class='pad-content'></div>")
     $(".responer-pic").last().append("<a href=''><div class='pad-avatar'><img src='background.jpg' alt='無此圖片' class='respons-picture'></div></a>")
     $(".pad-content").last().append("<div><div class='responser-name'>留言者1</div></div><div class='responser-context'><div class='text'></div></div><div class='response-day-button'><i class='fa-regular fa-clock'></i><span class='post-time'>XX 天前</span><span class='replay-btn'>回覆</span>")
     $(".text").last().append(textareaVal);
-    var Msgcontent = $("textarea:eq(0)").val()
-    $.ajax({
-        type: "POST",
-        url: "/PostPage/AddComment",
-        data: {
-            content: Msgcontent
-        },
-        success: function (result) {
-            alert("新增成功")
-        },
-        error: function (data) {
-            alert("新增失敗")
-        },
-    });
     $("textarea:eq(0)").val("");
     $("#commentbtn").attr("disabled", "true");
 
 });
-
-
 
 
 //新增回覆留言
@@ -55,7 +59,7 @@ $(".messageboard-frame").on("click", "[class=replay-btn]", function (e) {
         $(".comment-bother-input").last().append("<a href='' class='comment-pic-frame'></a><div class='textarea-box'></div><div class='col-auto-input'></div>")
         $(".comment-pic-frame").last().append("<div style='width: 24px; height: 24px;' class='avatar-frame'></div>")
         $(".avatar-frame").last().append(" <img src='background.jpg' alt='無此圖片' class='comment-bother-pic'>")
-        $(".textarea-box:eq(1)").append("<textarea type='text' class='form-control' id='exampleFormControlTextarea1' placeholder='請輸入你的訊息' maxlength='200' rows='3'></textarea><div class='statics-text'>0/200字</div>")
+        $(".textarea-box:eq(1)").append("<textarea type='text' class='form-control' id='exampleFormControlTextarea2' placeholder='請輸入你的訊息' maxlength='200' rows='3'></textarea><div class='statics-text'>0/200字</div>")
         $(".col-auto-input").append(" <button type='button' class='btn btn-primary' disabled data-bs-toggle='button' id='commentbtmre'>留言</button>")
     }
 
@@ -65,7 +69,7 @@ $(".messageboard-frame").on("click", "[class=replay-btn]", function (e) {
         $(".comment-bother-input").last().append("<a href='' class='comment-pic-frame'></a><div class='textarea-box'></div><div class='col-auto-input'></div>")
         $(".comment-pic-frame").last().append("<div style='width: 24px; height: 24px;' class='avatar-frame'></div>")
         $(".avatar-frame").last().append(" <img src='background.jpg' alt='無此圖片' class='comment-bother-pic'>")
-        $(".textarea-box:eq(1)").append("<textarea type='text' class='form-control' id='exampleFormControlTextarea1' placeholder='請輸入你的訊息' maxlength='200' rows='3'></textarea><div class='statics-text'>0/200字</div>")
+        $(".textarea-box:eq(1)").append("<textarea type='text' class='form-control' id='exampleFormControlTextarea2' placeholder='請輸入你的訊息' maxlength='200' rows='3'></textarea><div class='statics-text'>0/200字</div>")
         $(".col-auto-input").append(" <button type='button' class='btn btn-primary' disabled data-bs-toggle='button' id='commentbtmre'>留言</button>")
     }
 
