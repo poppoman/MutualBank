5ed5edb6c77e931d9e01ec66703212498a991b82
GailChang <107825061+GailChang@users.noreply.github.com>
2022-08-07 18:55:22 +0800

    1. 【修復】(刪除搜尋欄、登入欄造成的破版)  2. 點數- 首頁、新增、修改 完成!

--- MutualBank/Areas/Admin/Controllers/SkillsController.cs
+++ MutualBank/Areas/Admin/Controllers/SkillsController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using MutualBank.Areas.Admin.Models.ViewModel;
 using MutualBank.Models;
 
 namespace MutualBank.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/[controller]")]
     public class SkillsController : Controller
     {
         private readonly MutualBankContext _context;
@@ -20,41 +22,29 @@ namespace MutualBank.Areas.Admin.Controllers
         }
 
         // GET: Admin/Skills
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
-              return _context.Skills != null ? 
-                          View( _context.Skills) :
-                          Problem("找不到資料");
-        }
-
-        // GET: Admin/Skills/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Skills == null)
+            var skillModel = _context.Skills.Select(s => new SkillsIndex
             {
-                return NotFound();
-            }
-
-            var skill = await _context.Skills
-                .FirstOrDefaultAsync(m => m.SkillId == id);
-            if (skill == null)
-            {
-                return NotFound();
-            }
-
-            return View(skill);
+                SkillId = s.SkillId,
+                SkillName = s.SkillName,
+            }).ToList();
+              return (skillModel != null) ? 
+                     View("Index",skillModel) :Problem("找不到資料");
         }
 
         // GET: Admin/Skills/Create
+        [HttpGet]
+        [Route("Create")]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Admin/Skills/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Admin/Skills/Create        
         [HttpPost]
+        [Route("Create")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("SkillId,SkillName")] Skill skill)
         {
@@ -68,96 +58,61 @@ namespace MutualBank.Areas.Admin.Controllers
         }
 
         // GET: Admin/Skills/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        [HttpGet]
+        [Route("Edit/{id}")]
+        public async Task<IActionResult> Edit([FromRoute(Name = "id")]int? id)
         {
             if (id == null || _context.Skills == null)
             {
                 return NotFound();
             }
-
             var skill = await _context.Skills.FindAsync(id);
             if (skill == null)
             {
                 return NotFound();
             }
-            return View(skill);
+            var skillModel = new SkillsIndex
+            {
+                SkillId = skill.SkillId,
+                SkillName = skill.SkillName,
+            };
+            ViewBag.skillId = id;
+            ViewBag.skillName = skillModel.SkillName;
+            return View(skillModel);
         }
 
-        // POST: Admin/Skills/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Admin/Skills/Edit/5        
         [HttpPost]
+        [Route("Edit/{id}")]
+        [Produces("application/json")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("SkillId,SkillName")] Skill skill)
+        public IActionResult Edit(int id, [FromForm]SkillsIndex json)
         {
-            if (id != skill.SkillId)
+            var skillModel = _context.Skills.Where(u => u.SkillId == id).FirstOrDefault();
+            if ( !SkillExists(id) || skillModel == null)
             {
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
+            try
             {
-                try
-                {
-                    _context.Update(skill);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SkillExists(skill.SkillId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                _context.Skills.Update(CorrespondTheValue(skillModel, json));
+                _context.SaveChanges();
             }
-            return View(skill);
-        }
-
-        // GET: Admin/Skills/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Skills == null)
+            catch (DbUpdateConcurrencyException ex)
             {
-                return NotFound();
+                return NotFound(ex.Message);
             }
-
-            var skill = await _context.Skills
-                .FirstOrDefaultAsync(m => m.SkillId == id);
-            if (skill == null)
-            {
-                return NotFound();
-            }
-
-            return View(skill);
-        }
-
-        // POST: Admin/Skills/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Skills == null)
-            {
-                return Problem("Entity set 'MutualBankContext.Skills'  is null.");
-            }
-            var skill = await _context.Skills.FindAsync(id);
-            if (skill != null)
-            {
-                _context.Skills.Remove(skill);
-            }
-            
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
+        }        
 
         private bool SkillExists(int id)
         {
           return (_context.Skills?.Any(e => e.SkillId == id)).GetValueOrDefault();
+        }
+        private Skill CorrespondTheValue(Skill skillModel, SkillsIndex json)
+        {
+            skillModel.SkillName = json.SkillName;
+            return skillModel;
         }
     }
 }
---
+++ MutualBank/Areas/Admin/Models/ViewModel/SkillsIndex.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MutualBank.Areas.Admin.Models.ViewModel
+{
+    public class SkillsIndex
+    {
+        public int SkillId { get; set; }
+        [Display(Name = "技能名稱")]
+        public string SkillName { get; set; } = null!;
+    }
+}
