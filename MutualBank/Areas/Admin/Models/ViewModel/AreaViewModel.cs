9ddee9e516b7c3d1aaa35a034d53c64786f19a49
GailChang <107825061+GailChang@users.noreply.github.com>
2022-08-09 09:53:10 +0800

    Case Edit 畫面完成

---
+++ MutualBank/Areas/Admin/Models/ViewModel/AreaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MutualBank.Areas.Admin.Models.ViewModel
+{
+    public class AreaViewModel
+    {
+        public int AreaId { get; set; }
+        public string AreaCity { get; set; } = null!;
+        public string AreaTown { get; set; } = null!;
+        public string SerAreaDisplay { get; set; } = null!;
+        
+    }
+}
---
+++ MutualBank/Areas/Admin/Models/ViewModel/CaseApiModel.cs
@@ -0,0 +1,32 @@
+﻿namespace MutualBank.Areas.Admin.Models.ViewModel
+{
+    public class CaseApiModel
+    {
+        public int CaseId { get; set; }
+        public int CaseUserId { get; set; }
+        /// <summary>
+        /// 0提供 1需要
+        /// </summary>
+        public bool CaseNeedHelp { get; set; }
+        public int CaseSkilId { get; set; }
+        public DateTime CaseAddDate { get; set; }
+        public DateTime CaseReleaseDate { get; set; }
+        public DateTime CaseExpireDate { get; set; }
+        public DateTime? CaseClosedDate { get; set; }
+        public DateTime CaseClosedIn { get; set; }
+
+        public string? CaseCloseDisplay
+        {
+            set => CaseClosedIn.ToString("yyyy-MM-dd");
+            get => (CaseClosedIn.ToString("yyyy-MM-dd") != "2022-08-06") ? CaseClosedIn.ToString("yyyy-MM-dd") : "未結案";
+        }
+        public string CaseTitle { get; set; } = null!;
+        public string CaseIntroduction { get; set; } = null!;
+        public string? CasePhoto { get; set; }
+        public string? CaseSerDate { get; set; }
+        public int? CaseSerArea { get; set; }
+
+        public int CasePoint { get; set; }
+        public bool CaseIsExecute { get; set; }
+    }
+}
